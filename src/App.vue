875d9c7c91778910b444ac68561f771d0e022462
snow <136385621+snowcloudsx@users.noreply.github.com>
2025-05-18 14:15:32 -0400

    test
    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <Header />
-    <router-view />
+    <transition name="big-fade" mode="out-in">
+      <router-view />
+    </transition>
   </div>
 </template>
 
@@ -12,3 +14,23 @@ export default {
   components: { Header }
 };
 </script>
+
+<style scoped>
+.big-fade-enter-active, .big-fade-leave-active {
+  transition-property: opacity, transform;
+  transition-duration: 700ms;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1); /* ease-in-out */
+}
+.big-fade-enter-from {
+  opacity: 0;
+  transform: scale(0.9) translateY(2rem);
+}
+.big-fade-leave-to {
+  opacity: 0;
+  transform: scale(0.9) translateY(-2rem);
+}
+.big-fade-enter-to, .big-fade-leave-from {
+  opacity: 1;
+  transform: scale(1) translateY(0);
+}
+</style>
